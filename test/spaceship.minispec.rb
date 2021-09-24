461cd2b5e12889c45c0b10456670ea3e9c0365d1
nandorcsupor <csuprok@hotmail.com>
2021-09-24 11:02:18 +0200

    more tests

---
+++ test/spaceship.minispec.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+
+require_relative "../lib/module7"
+include Module7
+
+describe Spaceship do
+    before do
+        @ship = Spaceship.new("Serenity")
+    end
+
+    describe "when asked for a name" do
+        it "must provide on" do
+            @ship.name.must_equal "Serenity"
+        end
+    end
+end
+
